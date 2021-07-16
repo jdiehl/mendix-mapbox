8d306aa33b5f02cbb2a846548434ede620147352
Jonathan Diehl <jonathan.diehl@gmail.com>
2021-07-16 17:39:08 +0200

    Initial import

---
+++ typings/MapboxProps.d.ts
@@ -0,0 +1,32 @@
+/**
+ * This file was generated from Mapbox.xml
+ * WARNING: All changes made to this file will be overwritten
+ * @author Mendix UI Content Team
+ */
+import { CSSProperties } from "react";
+import { ListValue, ListAttributeValue } from "mendix";
+import { Big } from "big.js";
+
+export interface MapboxContainerProps {
+    name: string;
+    class: string;
+    style?: CSSProperties;
+    tabIndex?: number;
+    accessToken: string;
+    markerList?: ListValue;
+    latitudeAttr: ListAttributeValue<Big>;
+    longitudeAttr: ListAttributeValue<Big>;
+    width: string;
+    height: string;
+}
+
+export interface MapboxPreviewProps {
+    class: string;
+    style: string;
+    accessToken: string;
+    markerList: {} | null;
+    latitudeAttr: string;
+    longitudeAttr: string;
+    width: string;
+    height: string;
+}
